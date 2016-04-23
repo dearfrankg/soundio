4a7916957ba8ddae1d33b892848f3781309a928c
Frank Gutierrez <dearfrankg@gmail.com>
2016-04-22 18:30:35 -0700

    remove karma

--- test/karma/tests.webpack.js
+++
@@ -1,3 +0,0 @@
-// make sure you have your directory and regex test set correctly!
-var context = require.context('..', true, /\.spec\.js$/)
-context.keys().forEach(context)
--- test/sample.spec.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env mocha */
-var expect = require('expect')
-
-describe('sample test', function () {
-  it('should find that 1 equals 1', function () {
-    expect(1).toEqual(1)
-  })
-})
