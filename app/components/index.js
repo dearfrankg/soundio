3a632a6fd94b5108fa95990327478d10ad22ae5f
Frank Gutierrez <dearfrankg@gmail.com>
2016-04-26 23:10:37 -0700

    implement player

--- app/components/index.js
+++ app/components/index.js
@@ -2,10 +2,16 @@ import Link from './Link'
 import Nav from './Nav'
 import NavSearch from './NavSearch'
 import Popover from './Popover'
+import Player from './Player'
+import Playlist from './Playlist'
+import SongDetails from './SongDetails'
 
 export {
   Link,
   Nav,
   NavSearch,
-  Popover
+  Popover,
+  Player,
+  Playlist,
+  SongDetails
 }
--- app/containers/PlayerContainer.js
+++ app/containers/PlayerContainer.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import {
+  Player
+} from '../components'
+import { getPlayingSongId } from '../utils/PlayerUtils'
 
-export default class PlayerContainer extends React.Component {
+const propTypes = {
+  isMobile: PropTypes.bool,
+  playingSongId: PropTypes.number
+}
+
+class PlayerContainer extends Component {
+
   render () {
-    return (
-      <div>
-        PlayerContainer TBD
-      </div>
-    )
+    const { isMobile, playingSongId } = this.props
+    if (isMobile) {
+      return this.MobilePlayer()
+    }
+
+    if (playingSongId === null) {
+      return <div />
+    }
+
+    return <Player {...this.props} />
+  }
+
+  MobilePlayer () {
+    return null
+  }
+
+}
+
+PlayerContainer.propTypes = propTypes
+
+function mapStateToProps (state) {
+  const { entities, environment, player, playlists } = state
+  const { isMobile } = environment
+  const { songs, users } = entities
+  const playingSongId = getPlayingSongId(player, playlists)
+
+  return {
+    isMobile,
+    player,
+    playingSongId,
+    playlists,
+    songs,
+    users
   }
 }
+
+export default connect(mapStateToProps)(PlayerContainer)
