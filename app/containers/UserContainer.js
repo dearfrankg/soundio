acb5fe8d98fcf9386ab57314901dbafd87fa384b
Frank Gutierrez <dearfrankg@gmail.com>
2016-04-30 00:42:24 -0700

    implement user

--- app/containers/UserContainer.js
+++ app/containers/UserContainer.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getPlayingSongId } from '../utils/PlayerUtils'
+import { User } from '../components'
 
-export default class UserContainer extends React.Component {
+class UserContainer extends Component {
+
   render () {
-    return (
-      <div>
-        UserContainer TBD
-      </div>
-    )
+    return <User {...this.props} />
+  }
+
+}
+
+function mapStateToProps (state) {
+  const { authed, entities, environment, navigator, player, playlists } = state
+  const { height } = environment
+  const { songs, users } = entities
+  const { path } = navigator.route
+  const userId = Number(path[1])
+  const playingSongId = getPlayingSongId(player, playlists)
+
+  return {
+    authed,
+    height,
+    player,
+    playingSongId,
+    playlists,
+    songs,
+    userId,
+    users
   }
 }
+
+export default connect(mapStateToProps)(UserContainer)
